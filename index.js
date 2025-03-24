d6ba47c61873bc5d4d5bd3e1cddd0ff57bb4fe9b
subhranshu <subhranshu.mfs@gmail.com>
2025-03-24 15:54:15 +0530

    Added initial state and reducer

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ function orderCake(qty = 1) {
   }
 }
 
+const initialState = {
+  numOfCakes: 10,
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1
+      }
+    default:
+      return state
+  }
+}
+
